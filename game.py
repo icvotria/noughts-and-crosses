6ae8c8abfca3a46324701330e2102fcd09297065
unknown <icvotria@gmail.com>
2022-04-27 15:17:16 +0100

    check win function

--- game.py
+++ game.py
@@ -57,3 +57,33 @@ def draw_status():
     screen.blit(text, text_rect)
     
     pg.display.update()
+    
+def check_win():
+    global board, winner, draw
+
+    for row in range (0,3):
+        if ((board [row][0] == board[row][1] == board[row][2]) and (board [row][0] is not None)):
+            winner = board[row][0]
+            pg.draw.line(screen, (250,0,0), (0, (row + 1)*height/3 -height/6),\
+                                (width, (row + 1)*height/3 - height/6 ), 4)
+            break
+
+    for col in range (0, 3):
+        if (board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None):
+            winner = board[0][col]
+            pg.draw.line (screen, (250,0,0),((col + 1)* width/3 - width/6, 0),\
+                          ((col + 1)* width/3 - width/6, height), 4)
+            break
+
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
+        winner = board[0][0]
+        pg.draw.line (screen, (250,70,70), (50, 50), (350, 350), 4)
+
+    if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
+        winner = board[0][2]
+        pg.draw.line (screen, (250,70,70), (350, 50), (50, 350), 4)
+
+    if(all([all(row) for row in board]) and winner is None ):
+        draw = True
+    
+    draw_status()
