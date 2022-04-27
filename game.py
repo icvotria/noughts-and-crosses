35b02c647b9641ad0b9780667bd10f22654c9d33
unknown <icvotria@gmail.com>
2022-04-27 15:08:48 +0100

    game opening and draw status

--- game.py
+++ game.py
@@ -2,27 +2,58 @@ import pygame as pg,sys
 from pygame.locals import *
 import time
 
-def game():
-    player = 'x'
-    winner = None
-    draw = False
-    width = 400
-    height = 400
-    white = (255, 255, 255)
-    line_color = (10,10,10)
+player = 'x'
+winner = None
+draw = False
+width = 400
+height = 400
+white = (255, 255, 255)
+line_color = (10,10,10)
 
-    board = [[None]*3,[None]*3,[None]*3]
+board = [[None]*3,[None]*3,[None]*3]
 
-    pg.init()
-    fps = 30
-    clock = pg.time.Clock()
-    screen = pg.display.set_mode((width, height+100),0,32)
-    pg.display.set_caption("Noughts & Crosses")
+pg.init()
+fps = 30
+clock = pg.time.Clock()
+screen = pg.display.set_mode((width, height+100),0,32)
+pg.display.set_caption("Noughts & Crosses")
+
+opening = pg.image.load('opening.png')
+x_img = pg.image.load('x.png')
+o_img = pg.image.load('o.png')
+
+x_img = pg.transform.scale(x_img, (80,80))
+o_img = pg.transform.scale(o_img, (80,80))
+opening = pg.transform.scale(opening, (width, height+100))
+
+def game_opening():
+    screen.blit(opening,(0,0))
+    pg.display.update()
+    time.sleep(1)
+    screen.fill(white)
     
-    opening = pg.image.load('opening.png')
-    x_img = pg.image.load('x.png')
-    o_img = pg.image.load('o.png')
+    pg.draw.line(screen,line_color,(width/3,0),(width/3, height),7)
+    pg.draw.line(screen,line_color,(width/3*2,0),(width/3*2, height),7)
 
-    x_img = pg.transform.scale(x_img, (80,80))
-    o_img = pg.transform.scale(o_img, (80,80))
-    opening = pg.transform.scale(opening, (width, height+100))
+    pg.draw.line(screen,line_color,(0,height/3),(width, height/3),7)
+    pg.draw.line(screen,line_color,(0,height/3*2),(width, height/3*2),7)
+    draw_status()
+    
+def draw_status():
+    global draw
+    
+    if winner is None:
+        message = player.upper() + "'s Turn"
+    else:
+        message = winner.upper() + " won!"
+    if draw:
+        message = 'Game Draw!'
+    
+    font = pg.font.Font(None, 30)
+    text = font.render(message, 1, (255, 255, 255))
+
+    screen.fill ((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width/2, 500-50))
+    screen.blit(text, text_rect)
+    
+    pg.display.update()
