ed44c24c4b6368d918e40c65ffcfa74c816899bb
unknown <icvotria@gmail.com>
2022-04-27 14:31:12 +0100

    restart

--- game.py
+++ game.py
@@ -1,35 +1,3 @@
-theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
-            '4': ' ' , '5': ' ' , '6': ' ' ,
-            '1': ' ' , '2': ' ' , '3': ' ' }
-
-def printBoard(board):
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
-
-def game():
-    
-    turn = 'X'
-    count = 0
-    
-    for i in range(10):
-        printBoard(theBoard)
-        print(turn +"'s turn. Where do you want to go?")
-
-        move = input()        
-
-        if theBoard[move] == ' ':
-            theBoard[move] = turn
-            count += 1
-        else:
-            print("That place is already filled.\nWhere do you want to go?")
-            continue
-    
-        if turn =='X':
-            turn = 'O'
-        else:
-            turn = 'X'
-        
-game()
+import pygame as pg,sys
+from pygame.locals import *
+import time
