670475312a673c4629d5d06c988f8ec5e997520c
unknown <icvotria@gmail.com>
2022-04-27 14:35:01 +0100

    variables

--- game.py
+++ game.py
@@ -1,3 +1,11 @@
 import pygame as pg,sys
 from pygame.locals import *
 import time
+
+player = 'x'
+winner = None
+draw = False
+width = 400
+height = 400
+white = (255, 255, 255)
+line_color = (10,10,10)
