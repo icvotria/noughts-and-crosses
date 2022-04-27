2ca75771a818c1fbfd7b14747070b55ef1b54b04
unknown <icvotria@gmail.com>
2022-04-27 18:06:05 +0100

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Noughts & Crosses
+
+A simple noughts and crosses game made in Pygame.
+
+To play, clone the repo, navigate to the directory then run
+
+> python game.py
--- game.py
+++ game.py
@@ -10,30 +10,30 @@ draw = False
 width = 400
 height = 400
 white = (255, 255, 255)
-line_color = (10,10,10)
+line_color = (10, 10, 10)
 
-board = [[None]*3,[None]*3,[None]*3]
+board = [[None]*3, [None]*3, [None]*3]
 
 pg.init()
 fps = 30
 CLOCK = pg.time.Clock()
-screen = pg.display.set_mode((width, height+100),0,32)
+screen = pg.display.set_mode((width, height + 100), 0, 32)
 pg.display.set_caption("Noughts & Crosses")
 
 x_img = pg.image.load('x2.png')
 o_img = pg.image.load('o2.png')
 
-x_img = pg.transform.scale(x_img, (80,80))
-o_img = pg.transform.scale(o_img, (80,80))
+x_img = pg.transform.scale(x_img, (80, 80))
+o_img = pg.transform.scale(o_img, (80, 80))
 
 def game_opening():
     screen.fill(white)
     
-    pg.draw.line(screen, line_color, (width/3,0), (width/3, height), 7)
-    pg.draw.line(screen, line_color, (width/3*2,0), (width/3*2, height), 7)
+    pg.draw.line(screen, line_color, (width/3, 0), (width/3, height), 7)
+    pg.draw.line(screen, line_color, (width/3*2, 0), (width/3*2, height), 7)
 
-    pg.draw.line(screen, line_color, (0,height/3), (width, height/3) ,7)
-    pg.draw.line(screen, line_color, (0,height/3*2), (width, height/3*2), 7)
+    pg.draw.line(screen, line_color, (0, height/3), (width, height/3) ,7)
+    pg.draw.line(screen, line_color, (0, height/3*2), (width, height/3*2), 7)
     draw_status()
     
 def draw_status():
@@ -58,17 +58,17 @@ def draw_status():
 def check_win():
     global board, winner, draw
 
-    for row in range (0,3):
+    for row in range (0, 3):
         if ((board [row][0] == board[row][1] == board[row][2]) and (board [row][0] is not None)):
             winner = board[row][0]
-            pg.draw.line(screen, (250,0,0), (0, (row + 1)*height/3 -height/6),\
+            pg.draw.line(screen, (250, 0, 0), (0, (row + 1)*height/3 - height/6),\
                                 (width, (row + 1)*height/3 - height/6 ), 4)
             break
 
     for col in range (0, 3):
         if (board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None):
             winner = board[0][col]
-            pg.draw.line (screen, (250,0,0),((col + 1)* width/3 - width/6, 0),\
+            pg.draw.line (screen, (250,0,0), ((col + 1)* width/3 - width/6, 0),\
                           ((col + 1)* width/3 - width/6, height), 4)
             break
 
@@ -78,7 +78,7 @@ def check_win():
 
     if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
         winner = board[0][2]
-        pg.draw.line (screen, (250,70,70), (350, 50), (50, 350), 4)
+        pg.draw.line (screen, (250, 70, 70), (350, 50), (50, 350), 4)
 
     if(all([all(row) for row in board]) and winner is None ):
         draw = True
@@ -137,7 +137,7 @@ def userClick():
     if(row and col and board[row-1][col-1] is None):
         global player
 
-        drawMove(row,col)
+        drawMove(row, col)
         check_win()
         
 def reset_game():
