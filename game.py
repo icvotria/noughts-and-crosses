5cee1a9d07f5c1b99c4b666bfc59641883ff455c
unknown <icvotria@gmail.com>
2022-04-27 16:37:48 +0100

    better o and x

--- game.py
+++ game.py
@@ -21,24 +21,24 @@ screen = pg.display.set_mode((width, height+100),0,32)
 pg.display.set_caption("Noughts & Crosses")
 
 opening = pg.image.load('opening.png')
-x_img = pg.image.load('x.png')
-o_img = pg.image.load('o.png')
+x_img = pg.image.load('x2.png')
+o_img = pg.image.load('o2.png')
 
 x_img = pg.transform.scale(x_img, (80,80))
 o_img = pg.transform.scale(o_img, (80,80))
 opening = pg.transform.scale(opening, (width, height+100))
 
 def game_opening():
-    screen.blit(opening,(0,0))
-    pg.display.update()
-    time.sleep(1)
+    # screen.blit(opening,(0,0))
+    # pg.display.update()
+    # time.sleep(1)
     screen.fill(white)
     
-    pg.draw.line(screen,line_color,(width/3,0),(width/3, height),7)
-    pg.draw.line(screen,line_color,(width/3*2,0),(width/3*2, height),7)
+    pg.draw.line(screen, line_color, (width/3,0), (width/3, height), 7)
+    pg.draw.line(screen, line_color, (width/3*2,0), (width/3*2, height), 7)
 
-    pg.draw.line(screen,line_color,(0,height/3),(width, height/3),7)
-    pg.draw.line(screen,line_color,(0,height/3*2),(width, height/3*2),7)
+    pg.draw.line(screen, line_color, (0,height/3), (width, height/3) ,7)
+    pg.draw.line(screen, line_color, (0,height/3*2), (width, height/3*2), 7)
     draw_status()
     
 def draw_status():
